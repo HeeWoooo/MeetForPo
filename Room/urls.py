29413fb26bc14a85740467b83763492a36932232
HeeWoooo <hugm2hw@naver.com>
2022-08-27 14:56:00 +0900

    fixed

--- Room/urls.py
+++ Room/urls.py
@@ -3,5 +3,5 @@ from .views import RoomsAPIMixins, RoomAPIMixins
 
 urlpatterns = [
     path("mixin/rooms/", RoomsAPIMixins.as_view()),
-    path("mixin/room/<str:id>", RoomAPIMixins.as_view()),
+    path("mixin/rooms/<str:id>", RoomAPIMixins.as_view()),
 ]
