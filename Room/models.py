426731ac9c38eeabfe309ba7167f6f833f0b2ea8
HeeWoooo <hugm2hw@naver.com>
2022-08-28 22:17:50 +0900

    created room password

--- Room/models.py
+++ Room/models.py
@@ -11,8 +11,17 @@ def create_code(string_length = 10):
     random = random.upper()
     random = random.replace("=", "")
 
+
+
 class Room(models.Model):
 #    id = models.CharField(primary_key=True, default=create_code(8), editable=False, max_length=8)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, max_length=8)
     title = models.CharField(max_length = 50)
     max_people = models.PositiveIntegerField()
+    password = models.CharField(max_length=50, null=True)
+    password2 = models.CharField(max_length=50, null=True)
+#    head = models.ForeignKey(User, on_delete=models.CASADE)
+
+    #이렇게 하면 안되나 ?? => 접속 할때마다 추가되는지,, => 중복 허용 금지하는 게 있나
+    #members = models.ManyToManyField(User, related_name='my_rooms')
+ 
--- Room/serializers.py
+++ Room/serializers.py
@@ -5,4 +5,11 @@ from .models import Room
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ['id', 'title', 'max_people']
+        fields = ['id', 'title', 'max_people', 'password', 'password2']
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+        return data
